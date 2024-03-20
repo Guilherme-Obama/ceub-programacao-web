f34d7dc44123f86eec0106193c5bbfe9859c2b22
Guilherme-Obama <guiprosa@gmail.com>
2024-03-20 10:49:43 -0300

    final aula 20mar

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+let conteudo;
+
+conteudo = document.getElementById("titulos");
+
+conteudo.innerHTML = "<marquee> Esse conteudo está vindo do javascript</marquee>"
+
+let conteudo2 = document.getElementById("paragrafo-legal");
+conteudo2.innerHTML = "<p>esse paragrafo vem do javascript</p>"
+
+console.log(typeof(conteudo));
+console.log(conteudo);
+
+
+
+
+
+
+
+
+
+
+
+// Imprima no console do navegador os número de 1 a 5:
+//for (let numero = 1; numero < 6; numero++) {
+//    console.log(numero);
+//}
+
+/* let i = 1;
+
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+ */
+
+
 /* 
 let idade = 18;
 let nome = "joao"
